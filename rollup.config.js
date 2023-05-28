5700b017b8cebb1de0065a1623ecf71b51a47b26
sergiodaviduo <49468779+sergiodaviduo@users.noreply.github.com>
2023-05-28 10:13:16 +0000

    removed old rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,12 @@
 // rollup.config.js
-import clear from 'rollup-plugin-clear'
 
 export default {
 	input: 'src/app.js',
-    plugins: [
-        clear({
-            targets: ['mysite/music_training/static/js/']
-        })
-    ],
 	output: {
-        dir: 'mysite/src',
-		file: 'app.js',
+		file: 'mysite/music_training/static/js/app.js',
 		format: 'iife'
 	},
     watch: {
-        include: 'src/app.js'
+        input: 'src/**/*'
     }
 };
