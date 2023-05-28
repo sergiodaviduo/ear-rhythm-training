f991938cca341430f68ae58c4253edcc78b21410
sergiodaviduo <49468779+sergiodaviduo@users.noreply.github.com>
2023-05-28 23:55:12 +0000

    edited test to be accurate, was previously config stub

--- js_tests/score-test.js
+++ js_tests/score-test.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vitest/config'
+import { assert, expect, test } from 'vitest'
 
-export default defineConfig({
-  test: {
-    // ...
-  },
-})
+test('Math.sqrt()', () => {
+    expect(Math.sqrt(4)).toBe(2)
+    expect(Math.sqrt(144)).toBe(12)
+    expect(Math.sqrt(2)).toBe(Math.SQRT2)
+  })
