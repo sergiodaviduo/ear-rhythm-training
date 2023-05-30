7855f44df51b5e9eb453640e7e1d1f882f4d3282
sergiodaviduo <49468779+sergiodaviduo@users.noreply.github.com>
2023-05-30 13:43:03 +0000

    rewrote test a little to prep for future tests

--- js_test/game-test.js
+++ js_test/game-test.js
@@ -1,11 +1,14 @@
 import { assert, expect, test, vi } from 'vitest'
 import { Game } from '../src/components/game.js';
 
-// Defaults
-test.only('Game constructor returns expected defaults', () => {
-    const Spy = vi.fn(() => (new Game));
+const Spy = vi.fn(() => (new Game));
+const Mock = new Spy();
 
-    expect(Spy.mock.results[0].value).toEqual({
+// Defaults
+test('Game constructor returns expected defaults', () => {
+    const Construction = Spy.mock.results[0].value;
+
+    expect(Construction).toEqual({
         _score: 0,
         isPlaying: false,
         _inputWindowO: 0,
