27df9c2738a080f64cc2491e972a8a44a1d1c47e
fusion <surg.uroot@gmail.com>
2024-10-20 19:59:20 -0700

    yessssssssssssss added basic score submitting

--- mysite/music_training/views.py
+++ mysite/music_training/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 from music_training.forms import ScoreForm
 
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import loader
+
+# should process data in a different file then this at some point, but for now is ok
+from music_training.models import HighScores
 
 
 # whenever someone loads the homepage / root site, this method is ran:
@@ -13,32 +15,16 @@ def index(request):
     if request.method == "POST":
         # create a form instance and populate it with data from the request:
         form = ScoreForm(request.POST)
+        if form.is_valid():
+            print("Name of submitted form:"+form.cleaned_data["name"])
+            newScore = HighScores(name=form.cleaned_data["name"], finalScore=form.cleaned_data["final_score"])
 
-        print(form)
+            newScore.save()
+
+            print(form)
 
         return HttpResponseRedirect("/admin/")
     else:
         form = ScoreForm()
 
-    print("testhihi")
     return render(request, "music_training/index.html", {"form": form})
-
-'''def get_submitscore(request):
-    # if this is a POST request we need to process the form data
-    if request.method == "POST":
-        # create a form instance and populate it with data from the request:
-        form = ScoreForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect("/")
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = ScoreForm()
-
-    return render(request, "submitscore.html", {"form": form})
-
-'''
