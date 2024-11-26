2803f59cd54aff11695c9dd0742521a34b69a3c2
fusion <surg.uroot@gmail.com>
2024-11-25 20:17:29 -0800

    added obfuscator to rollup specifically

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 // rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
+import obfuscator from 'rollup-plugin-obfuscator';
 
 export default {
 	input: 'src/app.js',
@@ -9,8 +10,17 @@ export default {
 		format: 'iife',
 		assetFileNames: 'assets/../../css/animate.css'
 	},
-	plugins: [resolve(), css({output: "animate.css"}),],
-    watch: {
-        input: 'src/**/*'
-    }
+	plugins: 
+	[
+		resolve(), css({output: "animate.css"}),
+		obfuscator({
+			options: {
+				// Your javascript-obfuscator options here
+				// See what's allowed: https://github.com/javascript-obfuscator/javascript-obfuscator
+			},
+		}),
+	],
+	watch: {
+		input: 'src/**/*'
+	},
 };
