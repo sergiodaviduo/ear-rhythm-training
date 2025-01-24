0bf5813d467df27b08994061594ee8ba32e2cc4c
fusion <surg.uroot@gmail.com>
2025-01-23 17:18:16 -0800

    added dj database url implementation

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
