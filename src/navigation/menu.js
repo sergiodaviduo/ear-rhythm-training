3914b5e80b0a0ccf9b1d03b1b413842fca7cdc4b
fusion <surg.uroot@gmail.com>
2025-10-18 13:08:39 -0700

    consolidated settings function using markAsBlock

--- src/navigation/menu.js
+++ src/navigation/menu.js
@@ -54,17 +54,21 @@ export function settings(game) {
         all_elements_nl[i].style.display = "none";
     }
 
-    document.getElementById('liveTempo').innerHTML = game.tempo;
+    back_to_menu();
+
+    document.getElementById('liveTempo').innerHTML = game.tempo; //
     document.getElementById('liveTempo').value = game.tempo;
 
-    document.getElementById("back-to-menu").style.display = "block";
+    markAsBlock(document.getElementById("settings-values"));
+
+    /*
     document.getElementById("settings-values").style.display = "block";
     document.getElementById("delay").style.display = "block";
     document.getElementById("tempo").style.display = "block";
     document.getElementById("liveTempo").style.display = "block";
     document.getElementById("liveDelay").style.display = "block";
     document.getElementById("calibrate-delay").style.display = "block";
-    document.getElementById("default-settings").style.display = "block";
+    document.getElementById("default-settings").style.display = "block";*/
 }
 
 export function showScoreSubmitMenu(game) {
